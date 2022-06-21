393c524ff230411c44aa397112897e3ccac262f3
Alfred Boateng <programmingwithalfred@gmail.com>
2022-06-21 09:49:11 +0000

    Working version

--- src/modules/functions.js
+++ src/modules/functions.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
+import Swal from 'sweetalert2';
 
-const totalInput = document.querySelector('.total');
+// const totalInput = document.querySelector('.total');
 const commentsListContainer = document.querySelector('.comments-list');
 const nameInput = document.querySelector('#name');
 const commentInput = document.querySelector('#message');
@@ -9,22 +10,94 @@ const BaseURL =
   'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps';
 const APP_ID = 'QC6bnLUQwMT9GlJ8wh3Z';
 
-const loadComments = async () => {
+const loadComments = async (itemID = APP_ID) => {
   const { data } = await axios.request({
     method: 'get',
-    url: `${BaseURL}/${APP_ID}/comments?item_id=${APP_ID}`,
+    url: `${BaseURL}/${APP_ID}/comments?item_id=${itemID}`,
   });
 
   // set the total to the length of the list
-  totalInput.innerHTML = data.length;
+  // totalInput.innerHTML = data.length;
 
   // map the list as li on screen
-  data.forEach((item) => {
-    const li = document.createElement('li');
-    li.className = 'comment';
+  // data.forEach((item) => {
+  //   const li = document.createElement('li');
+  //   li.className = 'comment';
 
-    li.textContent = `${item.creation_date} ${item.username}: ${item.comment}`;
-    commentsListContainer.appendChild(li);
+  //   li.textContent = `${item.creation_date} ${item.username}: ${item.comment}`;
+  //   commentsListContainer.appendChild(li);
+  // });
+
+  return data;
+};
+
+const displayModal = (
+  imgURL = 'https://dailyguidenetwork.com/wp-content/uploads/2021/04/SIsta-Afia-620x406.jpg',
+  name = 'John legend',
+  itemID = 'QC6bnLUQwMT9GlJ8wh3Z',
+  fuel = 'titanium',
+  weight = 1324567890,
+  light = 897654678,
+  power = 7654
+) => {
+  const data = loadComments(itemID);
+  console.log('working now', data);
+
+  const li = document.createElement('li');
+
+  // data.forEach((item) => {
+  //   li.className = 'comment';
+
+  //   li.textContent = `${item.creation_date} ${item.username}: ${item.comment}`;
+  // });
+
+  Swal.fire({
+    title: '<strong>HTML <u>example</u></strong>',
+    html: `
+       <div class="model-container">
+      <div class="modal-img-container">
+        <img class="modal-img" src='${imgURL}' alt="modal" />
+        <h3 class="modal-header">${name}</h3>
+      </div>
+      <div class="modal-detail-container">
+        <p class="detail">Fuel: ${fuel}</p>
+        <p class="detail">Light: ${light}</p>
+        <p class="detail">Weight: ${weight}</p>
+        <p class="detail">Power: ${power}</p>
+      </div>
+
+      <div class="comments-container">
+        <div class="comment-header-container">
+          <h3 class="comment-header">Comments</h3>
+          <span class="total">${data.length}</span>
+        </div>
+        // <ul class="comments-list">${li}</ul>
+      </div>
+      <div class="comment-form">
+        <h3 class="form-header">Add a comment</h3>
+        <form action="#" class="form">
+          <input
+            type="text"
+            name="name"
+            id="name"
+            placeholder="Your name"
+            class="name-input"
+            required
+          />
+          <textarea
+            name="comment"
+            required
+            id="message"
+            rows="4"
+            placeholder="Your insights"
+            class="textarea-input"
+          ></textarea>
+          <input type="submit" value="Comment" class="btn" />
+        </form>
+      </div>
+    </div>
+      `,
+    showCloseButton: true,
   });
 };
 
@@ -47,4 +120,4 @@ const addComment = async (name, comment, item_id = APP_ID) => {
   return false;
 };
 
-export { loadComments, addComment, nameInput, commentInput };
+export { loadComments, addComment, nameInput, commentInput, displayModal };
