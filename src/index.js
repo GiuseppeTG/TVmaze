3314bf14765f7008706edeafdb7ee1655b428c17
Alfred Boateng <programmingwithalfred@gmail.com>
2022-06-21 11:35:37 +0000

    Add another example to the project

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
-import { loadComments, displayModal } from './modules/functions.js';
+import { displayModal } from './modules/functions.js';
 import './style.css';
 import './modal-styles.css';
 
-loadComments();
-
 document.querySelector('.launchModal').addEventListener('click', () => {
   displayModal();
+});
+
+document.querySelector('.launchAnotherModal').addEventListener('click', () => {
+  displayModal(
+    'https://upload.wikimedia.org/wikipedia/commons/d/d4/Lydia-Forson.jpg',
+    'Lydia mayaer',
+    'QC6bnLUQwMT9GlJ8wh3Z',
+    'Total',
+    4556789,
+    89,
+    89,
+  );
 });
