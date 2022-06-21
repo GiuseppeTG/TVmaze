69c0c5f70795ca390b300b7d26ef747f5a46912d
Alfred Boateng <programmingwithalfred@gmail.com>
2022-06-21 10:35:15 +0000

    Remove unused comments

--- src/index.js
+++ src/index.js
@@ -1,20 +1,6 @@
-import {
-  loadComments,
-  // addComment,
-  // nameInput,
-  // commentInput,
-  displayModal,
-} from './modules/functions.js';
+import { loadComments, displayModal } from './modules/functions.js';
 import './style.css';
 import './modal-styles.css';
-
-// const form = document.querySelector('.submit-form');
-
-// form.addEventListener('click', (e) => {
-//   console.log('claedfdghjkl;jhgfdghjk');
-//   addComment(nameInput.value, commentInput.value);
-//   e.preventDefault();
-// });
 
 loadComments();
 
