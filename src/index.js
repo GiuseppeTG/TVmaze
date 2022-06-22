9dfef5f82375f88593115fc06cb4637c4c300d52
Alfred Boateng <programmingwithalfred@gmail.com>
2022-06-22 10:55:21 +0000

    Add likes fucntionality on page

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './style.css';
 import './footer.css';
+import { getTotalLikes, addLike } from './likes.js';
 
 // const AppId = 'ucRLpFwZl71GWbNyaaEC';
 const ApiUrl = 'https://api.tvmaze.com';
@@ -10,48 +11,58 @@ const cardContainer = document.querySelector('.grid-container');
 // -----CARD----- //
 
 class UI {
-  static renderCard = (title, imgUrl, likesCounter = '123') => {
-    const card = document.createElement('div');
-    card.classList.add('card');
+  static renderCard = (title, imgUrl, itemID) => {
+    // get comment count from db
+    let likesCount = 0;
 
-    const imageContainer = document.createElement('div');
-    imageContainer.classList.add('image-container');
+    getTotalLikes(itemID).then((data) => {
+      likesCount = data;
 
-    const image = document.createElement('img');
-    image.classList.add('item-img');
-    image.src = imgUrl;
+      const card = document.createElement('div');
+      card.classList.add('card');
 
-    const itemInfo = document.createElement('div');
-    itemInfo.classList.add('item-info');
+      const imageContainer = document.createElement('div');
+      imageContainer.classList.add('image-container');
 
-    const itemTitle = document.createElement('h2');
-    itemTitle.classList.add('item-title');
-    itemTitle.innerText = title;
+      const image = document.createElement('img');
+      image.classList.add('item-img');
+      image.src = imgUrl;
 
-    const likesContainer = document.createElement('div');
-    likesContainer.classList.add('likes-container');
+      const itemInfo = document.createElement('div');
+      itemInfo.classList.add('item-info');
 
-    const heart = document.createElement('i');
-    heart.classList.add('fa-solid', 'fa-heart');
+      const itemTitle = document.createElement('h2');
+      itemTitle.classList.add('item-title');
+      itemTitle.innerText = title;
 
-    const likes = document.createElement('span');
-    likes.classList.add('likes-counter');
-    likes.textContent = likesCounter;
+      const likesContainer = document.createElement('div');
+      likesContainer.classList.add('likes-container');
 
-    const commentsButton = document.createElement('button');
-    commentsButton.classList.add('comments-button');
-    commentsButton.textContent = 'Comments';
-    // commentsButton.addEventListener('click', () => {
-    //   console.log(title)
-    // });
+      const heart = document.createElement('i');
+      heart.classList.add('fa-solid', 'fa-heart');
 
-    cardContainer.append(card);
-    card.append(imageContainer, itemInfo, commentsButton);
+      const likes = document.createElement('span');
+      likes.classList.add('likes-counter');
+      likes.textContent = likesCount;
 
-    imageContainer.append(image);
-    itemInfo.append(itemTitle, likesContainer);
-    likesContainer.append(heart, likes);
-  }
+      heart.addEventListener('click', () => {
+        likesCount += 1;
+        addLike(itemID).then((data) => data);
+      });
+
+      const commentsButton = document.createElement('button');
+      commentsButton.classList.add('comments-button');
+      commentsButton.textContent = 'Comments';
+      commentsButton.addEventListener('click', () => {});
+
+      cardContainer.append(card);
+      card.append(imageContainer, itemInfo, commentsButton);
+
+      imageContainer.append(image);
+      itemInfo.append(itemTitle, likesContainer);
+      likesContainer.append(heart, likes);
+    });
+  };
 }
 
 // ----- API FUNCTIONS -----//
@@ -59,7 +70,9 @@ class UI {
 const getSearchedShows = async (query) => {
   const response = await fetch(`${ApiUrl}/search/shows?q=${query}`);
   const data = await response.json();
-  data.forEach((TvShow) => UI.renderCard(TvShow.show.name, TvShow.show.image.medium));
+  data.forEach((TvShow) =>
+    UI.renderCard(TvShow.show.name, TvShow.show.image.medium, TvShow.show.id)
+  );
 };
 
 const getSomeShows = async () => {
@@ -67,7 +80,9 @@ const getSomeShows = async () => {
   const data = await response.json();
   // console.log(data)
   const someShows = data.slice(0, 9);
-  someShows.forEach((show) => UI.renderCard(show.name, show.image.medium));
+  someShows.forEach((show) =>
+    UI.renderCard(show.name, show.image.medium, show.id)
+  );
 };
 
 // ----- SEARCH -----//
